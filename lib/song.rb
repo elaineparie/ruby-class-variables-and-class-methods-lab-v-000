abf549fc4a1cc4a8013a16fb271e7d004d3cbb50
Elaine Anderson <eparie@bu.edu>
2018-05-31 18:33:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,16 +4,22 @@ attr_accessor :name, :artist, :genre
 
 @@count = 0
 
+@@genres = []
+
 def initialize(name, artist, genre)
     @@count += 1
     @name = name
     @artist = artist
     @genre = genre
+    @@genres << genre
   end
 
   def self.count
    @@count
  end
 
+def self.genres
+  @@genres 
+end 
 
 end
